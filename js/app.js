262ec0be7d25ab5b7c106e8f7677f71c94eab128
bmbsquared <60191699+bmbsquared@users.noreply.github.com>
2020-05-03 17:29:55 -0400

    Could not get javascript to work from this file, script directly in HTML

---
+++ js/app.js
@@ -0,0 +1,3 @@
+//could not get the javascript to work in this file
+//script is in the html file. I know this will  bring down my grade
+//but after trying many ways to get it to work this is how I was able to
